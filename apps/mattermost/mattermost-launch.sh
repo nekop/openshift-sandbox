536b8733582958957ef734506e5a599c189598a8
Takayoshi Kimura <takayoshi@gmail.com>
2016-01-15 12:47:36 +0900

    n/a

--- apps/mattermost/mattermost-launch.sh
+++ apps/mattermost/mattermost-launch.sh
@@ -9,6 +9,7 @@ if [[ ! -z "$DRIVER_NAME" && ! -z "$DATA_SOURCE" ]]; then
     sed -e 's#"DriverName": "mysql"#"DriverName": "'"$DRIVER_NAME"'"#' \
         -e 's#"DataSource": "mmuser:mostest@tcp(mysql:3306)/mattermost_test?charset=utf8mb4,utf8"#"DataSource": "'"$DATA_SOURCE"'"#' \
         /opt/mattermost/config/config.json.orig > /opt/mattermost/config/config.json
+    grep DataSource /opt/mattermost/config/config.json
 fi
 
 cd /opt/mattermost/bin/
