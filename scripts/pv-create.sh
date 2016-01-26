b2055848b3b9e4324b4ec338d3529127f6ce8ec8
Takayoshi Kimura <takayoshi@gmail.com>
2016-01-26 15:12:44 +0900

    n/a

---
+++ scripts/pv-create.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+oc project default
+for i in $(seq 1 10); do
+    PV=$(cat <<EOF
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: pv$(printf %04d $i)
+spec:
+  capacity:
+    storage: 5Gi
+  accessModes:
+    - ReadWriteOnce
+    - ReadWriteMany
+  nfs:
+    server: tkimura-ose-single.usersys.redhat.com
+    path: /exports/pv$(printf %04d $i)
+EOF
+)
+    echo "$PV" | oc create -f -
+    sudo mkdir /exports/pv$(printf %04d $i)
+done
+sudo chown -R nfsnobody:nfsnobody /exports
+sudo chmod -R 777 /exports
+
