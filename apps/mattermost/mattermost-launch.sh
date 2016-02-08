d923a6103fda250f10fb8f79ad99e609d466d5f1
Takayoshi Kimura <takayoshi@gmail.com>
2016-02-08 16:26:40 +0900

    Resolve database service name

--- apps/mattermost/mattermost-launch.sh
+++ apps/mattermost/mattermost-launch.sh
@@ -7,7 +7,7 @@ fi
 
 if [ "$DRIVER_NAME" = "mysql" ]; then
     sed -e 's#"DriverName": "mysql"#"DriverName": "'"$DRIVER_NAME"'"#' \
-        -e 's#"DataSource": "mmuser:mostest@tcp(mysql:3306)/mattermost_test?charset=utf8mb4,utf8"#"DataSource": "'"$MYSQL_USER:$MYSQL_PASSWORD@tcp($MYSQL_SERVICE_HOST:$MYSQL_SERVICE_PORT)/$MYSQL_DATABASE?charset=utf8mb4,utf8"'"#' \
+        -e 's#"DataSource": "mmuser:mostest@tcp(mysql:3306)/mattermost_test?charset=utf8mb4,utf8"#"DataSource": "'"$MYSQL_USER:$MYSQL_PASSWORD@tcp($(printenv ${DATABASE_SERVICE_NAME}_SERVICE_HOST):$(printenv ${DATABASE_SERVICE_NAME}_SERVICE_PORT))/$MYSQL_DATABASE?charset=utf8mb4,utf8"'"#' \
         /opt/mattermost/config/config.json.orig > /opt/mattermost/config/config.json
     grep DataSource /opt/mattermost/config/config.json
 fi
