368ec26607e8466e2db436bd1624a6575136257d
Takayoshi Kimura <takayoshi@gmail.com>
2016-02-10 10:45:36 +0900

    n/a

--- apps/mattermost/mattermost-launch.sh
+++ apps/mattermost/mattermost-launch.sh
@@ -5,6 +5,9 @@ if [ ! -f /opt/mattermost/config/config.json.orig ]; then
   cp -a /opt/mattermost/config/config.json /opt/mattermost/config/config.json.orig
 fi
 
+echo "${DATABASE_SERVICE_NAME}_SERVICE_HOST"
+echo "$(printenv ${DATABASE_SERVICE_NAME}_SERVICE_HOST)"
+
 if [ "$DRIVER_NAME" = "mysql" ]; then
     sed -e 's#"DriverName": "mysql"#"DriverName": "'"$DRIVER_NAME"'"#' \
         -e 's#"DataSource": "mmuser:mostest@tcp(mysql:3306)/mattermost_test?charset=utf8mb4,utf8"#"DataSource": "'"$MYSQL_USER:$MYSQL_PASSWORD@tcp($(printenv ${DATABASE_SERVICE_NAME}_SERVICE_HOST):$(printenv ${DATABASE_SERVICE_NAME}_SERVICE_PORT))/$MYSQL_DATABASE?charset=utf8mb4,utf8"'"#' \
