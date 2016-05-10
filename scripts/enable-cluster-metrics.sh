18b409b02350c1f9b8e5dba649bac99b041badaa
Takayoshi Kimura <takayoshi@gmail.com>
2016-05-10 09:59:02 +0900

    Minor fix

--- scripts/enable-cluster-metrics.sh
+++ scripts/enable-cluster-metrics.sh
@@ -2,6 +2,7 @@
 
 HAWKULAR_METRICS_HOSTNAME=hawkular-metrics.apps.shift.nekop.io
 
+oc project openshift-infra
 oc create -f - <<API
 apiVersion: v1
 kind: ServiceAccount
@@ -15,5 +16,5 @@ oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:opensh
 oc secrets new metrics-deployer nothing=/dev/null
 oc process -f /usr/share/openshift/examples/infrastructure-templates/enterprise/metrics-deployer.yaml -v HAWKULAR_METRICS_HOSTNAME=$HAWKULAR_METRICS_HOSTNAME,USE_PERSISTENT_STORAGE=false,REDEPLOY=true | oc create -f -
 
-echo "Make sure to add the follwoing to master-config.xml and restart master. Not the URL should end with '/hawkular/metrics'"
-echo "metricsPublicURL: $HAWKULAR_METRICS_HOSTNAME/hawkular/metrics"
+echo "Make sure to add the follwoing to master-config.xml and restart master. Note the URL should end with '/hawkular/metrics'"
+echo "metricsPublicURL: https://$HAWKULAR_METRICS_HOSTNAME/hawkular/metrics"
